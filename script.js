1cdc0e4f85682e61be4f29e943c6619af06075d9
Igor Dev <156615521+IgorYxz@users.noreply.github.com>
2024-01-27 08:06:01 -0300

    igor dev

--- script.js
+++
@@ -1,9 +0,0 @@
-function alertar() {
-  var feedback = prompt("Digite seu feedback:");
-  
-  if (feedback) {
-    alert("Obrigado pelo seu feedback: ");
-  } else {
-    alert("Você não inseriu nenhum feedback.");
-  }
-}
